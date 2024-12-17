4b6612585ff7899ac5ed115b2c3f9514fe148f37
alexdkfighter <13695242581@163.com>
2024-12-18 00:30:26 +0800

    Site updated: 2024-12-18 00:30:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1734452667779";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1734452995597";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
